85f5b8f9f0e47a0a35882a096c4519f53b98f370
Lam Ngo <ngobasonlam@gmail.com>
2018-04-06 22:59:49 -0400

    add helper functions to move the robot around in its memory

--- micromouse2018/final_code.ino
+++ micromouse2018/final_code.ino
@@ -56,7 +56,7 @@ typedef struct _command{
 //implement by a queue to simulate recursion on the robots
 QueueList<command> commands;
 
-int orientation[] = {1,2,3,4};
+int orientation[] = {1,2,4,8};
 
 //maze constant
 #define X 6
@@ -115,18 +115,18 @@ void int calcDistToGoal(int x, int y, int maze_dimension){
 coord getNewCoordinate(coord current, int orientation){
   coord newCoord = {0,0};
 
-  if (orientation == 0){
+  if (orientation == 1){
     newCoord.x = current.x;
     newCoord.y = current.y - 1;
-  } else if (orientation == 1){
-    newCoord.x = current.x - 1;
-    newCoord.y = current.y;
   } else if (orientation == 2){
-    newcoord.x = current.x + 1;
-    newCoord.y = current.y;
-  } else if (orientation == 3){
     newCoord.x = current.x;
     newCoord.y = current.y + 1;
+  } else if (orientation == 4){
+    newcoord.x = current.x + 1;
+    newCoord.y = current.y;
+  } else if (orientation == 8){
+    newCoord.x = current.x - 1;
+    newCoord.y = current.y;
   }
 
   return newCoord;
@@ -136,12 +136,12 @@ coord getNewCoordinate(coord current, int orientation){
 int getMinimumNeighbors(coord current){
   int minVal = -1;
 
-  for (int orient = 0; i < 4; i++){
+  for (int i = 0; i < sizeof(orientation); i++){
 
     //check if accessbile
 
-    if (maze[current.x][current.y].walls && orient != 0){
-      coord neighbor = getNewCoordinate(current, orient);
+    if (maze[current.x][current.y].walls && orientation[i] != 0){
+      coord neighbor = getNewCoordinate(current, orientation[i]);
 
       //if valid cell
 
@@ -188,7 +188,50 @@ bool win(coord current, coord[] goals){
         return true;
       }
     }
-    return false;    
+    return false;
   }
   return false;
+}
+
+//add a wall to a cell
+void updatecoord(coord current, int walldir){
+  if (isValid(current)){
+    if (maze[current.x][current.y].walls & walldir != 0){
+      maze[current.x][current.y].walls -= walldir;
+    }
+  }
+}
+
+int optimalDirection(coord current, int heading){
+  coord leastNext = {0,0};
+
+  int leastNextVal = sizeof(maze)*sizeof(maze);
+  int leastDir = heading;
+
+  if (maze[current.x][current.y].walls & heading != 0){
+    coord leastnextTemp = updatecoord(current, heading);
+
+    if (isValid(leastnextTemp)){
+      leastNext = leastnextTemp;
+      leastNextVal = maze[leastNext.x][leastNext.y].distance;
+    }
+  }
+
+  for (int i =0; i < sizeof(orientation); i++){
+    int dir = orientation[i];
+
+    if (maze[current.x][current.y].walls & dir != 0){
+      coord dirCoord = updatecoord(current, dir);
+
+      if (isValid(dirCoord)){
+        if (maze[dirCoord.x][dirCoord.y].distance < leastNextVal){
+          leastNextVal = maze[dirCoord.x][dirCoord.y].distance;
+          leastNext = dirCoord;
+          leastDir = dir;
+        }
+      }
+    }
+  }
+
+  return leastDir;
 }
